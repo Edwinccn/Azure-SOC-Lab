2833cab3a1e14bf33922136c064404e8c4f81be9
Edwinccn <162117956+Edwinccn@users.noreply.github.com>
2024-04-07 17:56:26 -0700

    Create Linux_SSH_FailLoginAttempt.ps1

---
+++ Attack Scripts/Linux_SSH_FailLoginAttempt.ps1
@@ -0,0 +1,20 @@
+Import-Module -Name SSHUtils
+$ServerIPAddress = "1.0.0.0" #IPAddress of host to connect to
+$username = "USERACCOUNT" #Name of account to login as
+$wrongpassword = "WRONGPASSWORD" 
+$rightpassword = "RIGHTPASSWORD" #Password to use to login successfully
+$FailAttempts = 10 #Number of times to fail logon
+
+
+for ($count = 1; $count -le $FailAttempts; $count++) {
+    Write-Host "Attempt $count - Connecting to $ServerIPAddress..."
+    try {
+        $session = New-SSHSession -ComputerName $ServerIPAddress -Credential (New-Object System.Management.Automation.PSCredential($username, (ConvertTo-SecureString $wrongpassword -AsPlainText -Force))) -ErrorAction Stop
+    } catch {
+        Write-Host "Connection failed with error: $($_.Exception.Message)"
+    }
+}
+#Below is optional if you want to have a successful attempt after the fail login attempts
+$session = New-SSHSession -ComputerName $ServerIPAddress -Credential (New-Object System.Management.Automation.PSCredential($username, (ConvertTo-SecureString $rightpassword -AsPlainText -Force))) -ErrorAction Stop
+Write-Host "Connected"
+Remove-SSHSession -SessionId $session.SessionId
